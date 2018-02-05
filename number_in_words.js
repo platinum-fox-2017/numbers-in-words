c39ccff952dd7c2df95f5ff76b9aab12af7e4b83
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-05 19:52:38 +0700

    release 1

--- number_in_words.js
+++ number_in_words.js
@@ -7,55 +7,59 @@ var listNumberandWord = [
     {number: 6, word : 'enam'},
     {number: 7, word : 'tujuh'},
     {number: 8, word : 'delapan'},
-    {number: 9, word : 'sembilan'},
-    {number: 10, word : 'sepuluh'},
-    {number: 11, word : 'sebelas'},
-    {number: 12, word : 'dua belas'},
-    {number: 13, word : 'tiga belas'},
-    {number: 14, word : 'empat belas'},
-    {number: 15, word : 'lima belas'},
-    {number: 16, word : 'enam belas'},
-    {number: 17, word : 'tujuh belas'},
-    {number: 18, word : 'delapan belas'},
-    {number: 19, word : 'sembilan belas'}
+    {number: 9, word : 'sembilan'}
   ];
 
 function numberToWords(number) {
-  var result = '';
+	var strNumber = number.toString();
 
-	while (number > 0) {
-
-		if (number < 20) {
-			for (var i = 0; i < listNumberandWord.length; i++) {
-				if (listNumberandWord[i].number == number) {
-					result += listNumberandWord[i].word;
-					number -= listNumberandWord[i].number;
-				}
-			}
-		} else if (number < 100) {
-			for (var i = 8; i >= 2; i--) {
-				if (listNumberandWord[i].number == Math.floor(number/10)) {
-					result += listNumberandWord[i].word + ' puluh ';
-					number %= 10;
-					break;
-				}
-			}
-		} else if (number > 99 && number < 200) {
-			result += 'seratus ';
-			number %= 100;
-		} else if (number < 1000) {
-			for (var i = 8; i >= 1; i--) {
-				if (listNumberandWord[i].number == Math.floor(number/100)) {
-					result += listNumberandWord[i].word + ' ratus ';
-					number %= 100;
-					break;
-				}
-			}
-		}
-	}
-
-  return result;
+	if (number == 0) return '';
+	if (strNumber.length == 1) return listNumberandWord[number - 1].word; else
+	if (strNumber.length == 2) {
+		if (number < 20) return belasan(number); else
+		if (number < 100) return puluhan(number) + numberToWords(number % 10);
+	} else
+	if (strNumber.length == 3) return ratusan(number) + numberToWords(number % 100); //else
+	// if (strNumber.length < 7) return ribuan(number) + numberToWords(number % 1000);
 }
+
+function belasan(number) {
+	if (number % 10 == 1) return 'sebelas ';
+	else return listNumberandWord[(number % 10) - 1].word + ' belas ';
+}
+
+function puluhan(number) {
+	if (number / 10 == 1) return 'sepuluh ';
+	else return listNumberandWord[Math.floor(number / 10) - 1].word + ' puluh ';
+}
+
+function ratusan(number) {
+	if (Math.floor(number / 100) == 1) return 'seratus ';
+	else return listNumberandWord[Math.floor(number / 100) - 1].word + ' ratus ';
+}
+
+// function ribuan(number) {
+// 	var ribu = Math.floor(number / 1000);
+// 	if (ribu == 1) return 'seribu ';
+// 	else {
+// 		if (ribu > 10 && ribu < 20) return belasan(ribu) + 'ribu '; else
+// 		if (ribu < 99) return puluhan(ribu) + 'ribu '; else
+// 		return ratusan(ribu) + 'ribu ';
+// 	}
+// }
+
+// function jutaan(argument) {
+// 	var juta = Math.floor(number / 1000);
+	
+// }
+
+// function milyaran(argument) {
+// 	// body...
+// }
+
+// function triliunan(argument) {
+// 	// body...
+// }
 
 // Driver code
 console.log(numberToWords(1));
@@ -65,5 +69,6 @@ console.log(numberToWords(100));
 console.log(numberToWords(200));
 console.log(numberToWords(111));
 console.log(numberToWords(999));
-// console.log(numberToWords(1000000));
+// console.log(numberToWords(1111));
+// console.log(numberToWords(111.111));
 // console.log(numberToWords(2011845));
