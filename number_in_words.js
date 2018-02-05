de6e0eb6fd89ece9a4c630007a6c66feea780ae9
root <irsyadpahlapi28@gmail.com>
2018-02-05 07:53:43 -0500

    upload file number in word js

--- number_in_words.js
+++ number_in_words.js
@@ -1,5 +1,41 @@
 function numberToWords(number) {
-  // Your code here
+
+    var bilangan =['','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas']
+
+    if(number===0){
+      return ''
+    }
+    else if(number<12){
+      return bilangan[number]
+    }
+    else if(number<20){
+      return bilangan[number.toString()[1]]+'belas'
+    }
+    else{
+      if(number<100){
+        return numberToWords(Math.floor(number/10))+' puluh '+numberToWords(Number(number.toString().slice(-1)))
+      }
+      else if(number<1000){
+        return numberToWords(Math.floor(number/100))+' ratus '+numberToWords(Number(number.toString().slice(-2)))
+      }
+      else if(number<10000){
+        return numberToWords(Math.floor(number/1000))+' ribu '+numberToWords(Number(number.toString().slice(-3)))
+      }
+      else if(number<100000){
+        return numberToWords(Math.floor(number/1000))+' ribu '+numberToWords(Number(number.toString().slice(-3)))
+      }
+      else if(number<1000000){
+        return numberToWords(Math.floor(number/100000))+' ratus '+numberToWords(Number(number.toString().slice(-5)))
+      }
+      else if(number<10000000){
+        if(Math.floor(number/1000000)===1){
+            return 'sejuta '+numberToWords(Number(number.toString().slice(-6)))
+        }
+        else{
+            return numberToWords(Math.floor(number/1000000))+' juta '+numberToWords(Number(number.toString().slice(-6)))
+        }
+      }
+    }
 }
 
 // Driver code
