d508ea3c83dc19e2232bb496f2339809749680f8
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-05 15:59:22 +0700

    release 1 & 2

--- number_in_words.js
+++ number_in_words.js
@@ -18,15 +18,6 @@ function numberToWords(num) {
 
   else if (num < 1000) {
     tempWords = satuan[Math.floor(num/100)] + ' ratus ' + numberToWords(num%100)
-
-    //RELEASE 0 WITHOUT RECURSIVE
-
-    // if (num%100) {
-    //   tempWords += ' ratus ' + puluhan[Math.floor((num%100) /10) | 0]
-    // }
-    // if (num%10) {
-    //   tempWords += ' ' + satuan[num%10]
-    // }
   }
 
   else if (num < 10000) {
@@ -41,18 +32,45 @@ function numberToWords(num) {
     tempWords = satuan[Math.floor(num/100000)] + ' ratus ' + numberToWords(num%100000)
   }
 
+  else if (num < 10000000) {
+    tempWords = satuan[Math.floor(num/1000000)] + ' juta ' + numberToWords(num%1000000)
+  }
 
+  else if (num < 100000000) {
+    tempWords = puluhan[Math.floor(num/10000000)] + ' '+ numberToWords(num%10000000)
+  }
 
+  else if (num < 1000000000) {
+    tempWords = satuan[Math.floor(num/100000000)] + ' miliar '+ numberToWords(num%10000000)
+  }
 
+  else if (num < 10000000000) {
+    tempWords = satuan[Math.floor(num/1000000000)] + ' triliun '+ numberToWords(num%1000000000)
+  }
 
+  else if (num < 100000000000) {
+    tempWords = puluhan[Math.floor(num/10000000000)] + ' '+ numberToWords(num%10000000000)
+  }
+
+  else if (num < 1000000000000) {
+    tempWords = satuan[Math.floor(num/100000000000)] + ' ratus '+ numberToWords(num%100000000000)
+  }
+
+  if (tempWords.slice(0,5) === 'satu') {
+    tempWords = tempWords.replace('satu ','se')
+  }
+
+  // console.log(Math.floor(num/10000000));
   return tempWords
 }
 
 
-// console.log(numberToWords(765));
+
 
 
 // Driver code
-console.log(numberToWords(705));
-console.log(numberToWords(1000000));
-console.log(numberToWords(2011845));
+console.log(numberToWords(10));
+// console.log(numberToWords(765));
+// console.log(numberToWords(31));
+// console.log(numberToWords(1000000));
+// console.log(numberToWords(2011845));
