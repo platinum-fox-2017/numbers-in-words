c64425bc97a9cc6ebc2f21d1439cd49c785740ca
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-05 15:50:02 +0700

    Release 1

--- number_in_words.js
+++ number_in_words.js
@@ -1,6 +1,7 @@
 function numberToWords(number) {
   // Your code here
   let words = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
+  // index
   let satuan = number.toString().charAt(2)
   let belas = number.toString().charAt(1)
   let puluh = number.toString().charAt(1)
@@ -9,30 +10,38 @@ function numberToWords(number) {
   if (number < 12) { // satuan
     return words[number]
   } else if (number < 20) { // belasan
-    return words[number.toString().charAt(1)] + " belas "
+    return numberToWords(belas) + " belas"
+    // return words[number.toString().charAt(1)] + " belas "
   } else if (number < 100) {
-    return words[number.toString().charAt(0)] + " puluh " + words[number.toString().charAt(1)]
+    return numberToWords(ratus) + " puluh " + numberToWords(number.toString().charAt(1))
+    // return words[number.toString().charAt(0)] + " puluh " + words[number.toString().charAt(1)]
   } else if (number < 112) {
-    return "seratus " + words[parseInt(belas + satuan)]
+    return "seratus " + numberToWords(parseInt(belas + satuan))
+    // return "seratus " + words[parseInt(belas + satuan)]
   } else if (number < 120) {
-    return "seratus " + words[satuan] + " belas"
+    return "seratus " + numberToWords(satuan) + " belas"
+    // return "seratus " + words[satuan] + " belas"
   } else if (number < 200) { // 120 to 199
-    return "seratus " + words[puluh] + " puluh " + words[satuan]
+    return "seratus " + numberToWords(puluh) + " puluh " + numberToWords(satuan)
   } else if (number < 1000) { // 200 to 999
     if (puluh === '0') {
-      return words[ratus] + ' ratus ' + words[puluh] + words[satuan]
+      return numberToWords(ratus) + ' ratus ' + numberToWords(puluh) + numberToWords(satuan)
     } else if (puluh === '1') {
-      return words[ratus] + ' ratus ' + words[parseInt(satuan)] + ' belas '
+      return numberToWords(ratus) + ' ratus ' + numberToWords(parseInt(satuan)) + ' belas '
     } else {
-      return words[ratus] + ' ratus ' + words[parseInt(puluh)] + ' puluh ' + words[parseInt(satuan)]
+      return numberToWords(ratus) + ' ratus ' + numberToWords(parseInt(puluh)) + ' puluh ' + numberToWords(parseInt(satuan))
     }
   }
 }
 
 // Driver code
 console.log(numberToWords(4));
+console.log(numberToWords(14));
 console.log(numberToWords(27));
+console.log(numberToWords(112));
 console.log(numberToWords(102));
+console.log(numberToWords(142));
 console.log(numberToWords(705));
+console.log(numberToWords(727));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
