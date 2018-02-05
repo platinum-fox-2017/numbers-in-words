f37cacda5d9560d7cce024d20182500fa7ab9b94
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-05 15:20:20 +0700

    Refactor Release 0

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,10 @@
 function numberToWords(number) {
   // Your code here
   let words = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
-  let seratus = 'seratus'
-  let belas = 'belas'
-  let puluh = 'puluh'
+  let satuan = number.toString().charAt(2)
+  let belas = number.toString().charAt(1)
+  let puluh = number.toString().charAt(1)
+  let ratus = number.toString().charAt(0)
   // loop selama words length
   if (number < 12) { // satuan
     return words[number]
@@ -12,20 +13,12 @@ function numberToWords(number) {
   } else if (number < 100) {
     return words[number.toString().charAt(0)] + " puluh " + words[number.toString().charAt(1)]
   } else if (number < 112) {
-    let satuan = number.toString().charAt(2)
-    let belas = number.toString().charAt(1)
     return "seratus " + words[parseInt(belas + satuan)]
   } else if (number < 120) {
-    let satuan = number.toString().charAt(2)
-    return seratus + " " + words[satuan] + " " + belas
+    return "seratus " + words[satuan] + " belas"
   } else if (number < 200) { // 120 to 199
-    let puluh = number.toString().charAt(1)
-    let satuan = number.toString().charAt(2)
-    return seratus + " " + words[puluh] + " puluh " + words[satuan]
+    return "seratus " + words[puluh] + " puluh " + words[satuan]
   } else if (number < 1000) { // 200 to 999
-    let ratus = number.toString().charAt(0)
-    let puluh = number.toString().charAt(1)
-    let satuan = number.toString().charAt(2)
     if (puluh === '0') {
       return words[ratus] + ' ratus ' + words[puluh] + words[satuan]
     } else if (puluh === '1') {
@@ -37,6 +30,9 @@ function numberToWords(number) {
 }
 
 // Driver code
+console.log(numberToWords(4));
+console.log(numberToWords(27));
+console.log(numberToWords(102));
 console.log(numberToWords(705));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
