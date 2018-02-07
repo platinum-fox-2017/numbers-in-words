7b0ca96d0230099447f708e35e4cf8cd2d191da2
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 23:03:27 +0700

    NUMBER IN WORDS 3 DIGITS

--- number_in_words.js
+++ number_in_words.js
@@ -1,8 +1,35 @@
 function numberToWords(number) {
   // Your code here
+  var words='';
+  var satuan=['','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas','dua belas','tiga belas','empat belas','lima belas','enam belas','tujuh belas','delapan belas','sembilan belas','dua puluh'];
+  var puluhan=['','','dua puluh','tiga puluh','empat puluh','lima puluh', 'enam puluh','tujuh puluh','delapan puluh','sembilan puluh']
+  if(number<20){
+    words=satuan[number];
+  }
+  else if(number>=20 && number<100){
+    if(number%10===0){
+      words=puluhan[Math.floor(number/10)];
+    }
+    else{
+      words=puluhan[Math.floor(number/10)]+' '+satuan[number%10];
+    }
+  }
+  else if(number>99 && number<1000){
+      if(number===100){
+        words='seratus';
+      }
+      else {
+        words=satuan[Math.floor(number/100)]+' ratus '+numberToWords(number%100);
+      }
+
+  }
+  else{
+    words="Out Of Range";
+  }
+  return words;
 }
 
 // Driver code
-console.log(numberToWords(705));
-console.log(numberToWords(1000000));
-console.log(numberToWords(2011845));
+console.log(numberToWords(911));
+//console.log(numberToWords(1000000));
+//console.log(numberToWords(2011845));
