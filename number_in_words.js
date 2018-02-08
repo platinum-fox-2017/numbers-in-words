e691435b7deab46c90691e99cc9f3a669449d9db
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-02-08 08:12:49 +0700

    done

--- number_in_words.js
+++ number_in_words.js
@@ -3,54 +3,119 @@
 // Gunakan rekursif untuk pengurangan numbernya
 
 function numberToWords(number) {
-  
-  let oneDigits = ['', 'satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan'] 
-  // let twoDigitsBelas = ['sepuluh', 'sebelas','duabelas','tigabelas','empatbelas','limabelas','enambelas','tujuhbelas','delapanbelas','sembilanbelas']
-  let oneMultiplier = ['se','se', 'dua','tiga','empat','lima','enam','tujuh','delapan','sembilan']
-  let multiplier = ['belas','satu ', 'puluh', 'ratus', 'ribu','puluh','ratus','juta','puluh','ratus','trilliun']
+  let satuan = ['','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh', 'sebelas']
 
-  if(number === 0 ){
-    return ''
-  }else{
-    let numbString = String(number)
-    
-    // Check Angka 1
-    if(numbString.length < 2){
-      
-      return oneDigits[number] + numberToWords(number - number)
-      // Check belas
-    }else if(Number(numbString[0]) === 1 && numbString.length === 2){
-      if(number - 10 === 0){
-        return oneMultiplier[number-10] + multiplier[2] +  numberToWords (number - number) + " "
-      }else{
-        return oneMultiplier[number-10] + multiplier[0] +  numberToWords (number - number) + " "
-      }
-    // Ratusan untuk yang angkadepan 1
-    }else if(Number(numbString[0]) === 1 && numbString.length > 2){
-      
-      let angkaDepan = Number(numbString[0])
-      let multiply = Math.pow(10,numbString.length-1)
-      let pengurangan = number - (angkaDepan*multiply)
-      
-      return oneMultiplier[0] + multiplier[numbString.length] + " " + numberToWords(pengurangan)
-    }
-    // Puluhan selain angka depan 1
-    if(numbString.length > 1){
-      
-      let angkaDepan = Number(numbString[0])
-      let multiply = Math.pow(10,numbString.length-1)
-      let pengurangan = number - (angkaDepan*multiply)
-      
-      return oneDigits[angkaDepan] + " " + multiplier[numbString.length] + " " + numberToWords(pengurangan)
-    }
-    
+  if(number < 12){
+    return satuan[number]
+  }else if(number < 20){
+    return numberToWords(number % 10) + "belas"
+  }else if(number < 100){
+    return numberToWords(Math.floor(number/ 10)) + " puluh " + numberToWords(number%10)
+  }else if(number < 200){
+    return "seratus " + numberToWords(number % 100)
+  }else if(number < 1000){
+    return numberToWords(Math.floor(number/100)) + " ratus " + numberToWords(number%100)
+  }else if(number < 2000){
+    return "seribu " + numberToWords(number % 1000)
+  }else if(number < 1000000){
+    return numberToWords(Math.floor(number/1000)) + " ribu " + numberToWords(number%1000)
+  } else if(number < 1000000000){
+    return numberToWords(Math.floor(number/1000000)) + " juta " + numberToWords(number%1000000)
+  } else if(number < 1000000000000){
+    return numberToWords(Math.floor(number/1000000000)) + " milliar " + numberToWords(number%1000000000)
+  }else if(number < 1000000000000000){
+    return numberToWords(Math.floor(number/1000000000000)) + " trilliun " + numberToWords(number%1000000000000)
   }
+}
+
+function trimming(numb){
+  let convert = numberToWords(numb)
+  let wordsSplit = convert.split(" ")
+  const newArrayWords = wordsSplit.filter(word=>{
+    if(word !== ""){
+      return word
+    }
+  })
+
+  return newArrayWords.join(" ")
 }
 
 // Driver cod
 
-console.log(numberToWords(19999));
-// console.log(numberToWords(1000000));
-// console.log(numberToWords(2011845));
+// console.log(numberToWords(4));
+// console.log(numberToWords(12));
+// console.log(numberToWords(24));
+// console.log(numberToWords(114));
+// console.log(numberToWords(914));
+// console.log(numberToWords(1121));
+// console.log(numberToWords(212222));
+console.log(trimming(1200000));
+console.log(trimming(1200000000));
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let oneDigits = ['', 'satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan'] 
+//   // let twoDigitsBelas = ['sepuluh', 'sebelas','duabelas','tigabelas','empatbelas','limabelas','enambelas','tujuhbelas','delapanbelas','sembilanbelas']
+//   let oneMultiplier = ['se','sebelas', 'duabelas','tigabelas','empatbelas','limabelas','enambelas','tujuhbelas','delapanbelas','sembilanbelas']
+//   let multiplier = ['puluh','puluh', 'ratus', 'ribu','puluh','ratus', 'ribu','juta','puluh','ratus', 'ribu','trilliun','puluh','ratus','ribu']
+
+
+
+// let numbString = String(number)
+    
+    // // Check Angka 1
+    // if(numbString.length < 2){
+      
+    //   return oneDigits[number] + numberToWords(number - number)
+    // // Ratusan untuk yang angkadepan 1
+    // }else if(Number(numbString[0]) === 1 && numbString.length > 1){
+    //   let length = numbString.length
+      
+    //   let depan = Math.floor(number / Math.pow(10,numbString.length-2))
+
+    //   let angkaDepan = Number(numbString.slice(0,2))
+    //   let multiply = Math.pow(10,numbString.length-2)
+      
+    //   let pengurangan = number - (angkaDepan*multiply)
+      
+    //   if(numbString.length > 2){
+        
+    //     return oneMultiplier[depan-10] + " " + multiplier[numbString.length+1] + " " + numberToWords(pengurangan)
+    //   }else{
+        
+    //     return oneMultiplier[depan-10]
+    //   } 
+    // }
+    // // Puluhan selain angka depan 1
+    // if(numbString.length > 1){
+      
+    //   let angkaDepan = Number(numbString[0])
+    //   let multiply = Math.pow(10,numbString.length-1)
+    //   let pengurangan = number - (angkaDepan*multiply)
+      
+    //   console.log(angkaDepan*multiply)
+      
+    //   return oneDigits[angkaDepan] + " " + multiplier[numbString.length-1] + " " + numberToWords(pengurangan)
+    // }
